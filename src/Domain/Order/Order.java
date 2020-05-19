c332429481d9d65d6c52fbc897f6f489bc4dc765
JacobBonefeld <jacobbonefeld@live.dk>
2020-05-19 11:44:06 +0200

    - Added initial design for Driver GUI - Added OrderManager, Order and OrderItem skeleton

--- src/Domain/Order/Order.java
+++ src/Domain/Order/Order.java
@@ -1,23 +1,20 @@
 package Domain.Order;
 
-import Foundation.Database.DB;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Order {
 
     private int ID;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
     private String status;
     private int deliveryPointID;
     private int customerID;
     private ArrayList<OrderItem> items;
 
-    public Order(int ID, LocalDateTime startDate, LocalDateTime endDate, String status, int deliveryPointID, int customerID) {
-        this.ID = ID;
+    public Order(int ID, LocalDate startDate, LocalDate endDate, String status, int deliveryPointID, int customerID){
+        this.ID =  ID;
         this.startDate = startDate;
         this.endDate = endDate;
         this.status = status;
@@ -27,29 +24,11 @@ public class Order {
         this.items = new ArrayList<>();
     }
 
-    public void updateStatus(String status) {
+    public void updateStatus(String status){
 
     }
 
-    /**
-     * @return Status message of the order
-     */
-    public String getStatusMessage() {
-        DB.selectSQL("SELECT * FROM getOrderStatus(" + this.status + ")");
-        return DB.getData();
-    }
-
-    public ArrayList<OrderItem> getOrderItems() {
+    public ArrayList<OrderItem> getOrderItems(){
         return this.items;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderID: " + this.ID + " Start date: " + this.startDate + " End date " + this.endDate +
-                " Status: " + this.status + " DeliveryPointID " + this.deliveryPointID + " CustomerID " + this.customerID;
     }
 }
--- src/Domain/Order/OrderItem.java
+++ src/Domain/Order/OrderItem.java
@@ -1,33 +1,19 @@
 package Domain.Order;
 
-import java.time.LocalDateTime;
-
 public class OrderItem {
 
     private boolean isWashed;
     private int ID;
-    private int orderID;
-    private int laundryItemID;
-    private LocalDateTime startDateTime;
-    private LocalDateTime endDateTime;
+    private String type;
 
-    public OrderItem(int ID, int laundryItemID, int orderID, boolean isWashed, LocalDateTime startDateTime, LocalDateTime endDateTime){
+    public OrderItem(boolean isWashed, int ID, String type){
         this.isWashed = isWashed;
         this.ID =  ID;
-        this.laundryItemID = laundryItemID;
-        this.orderID = orderID;
-        this.startDateTime = startDateTime;
-        this.endDateTime = endDateTime;
+        this.type = type;
     }
 
     public void updateStatus(boolean status){
 
-    }
-
-    @Override
-    public String toString() {
-        return "ID: " + ID + " OrderID: " + orderID + " laundryItemID " + laundryItemID + " isWashed " + isWashed +
-                " start date " + startDateTime + " end date " + endDateTime;
     }
 
     public boolean isWashed() {
@@ -38,5 +24,7 @@ public class OrderItem {
         return ID;
     }
 
-
+    public String getType() {
+        return type;
+    }
 }
