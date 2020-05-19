3913e03b92bea1d47884e36b3b6a28da0bbe4b3e
JacobBonefeld <jacobbonefeld@live.dk>
2020-05-20 01:04:57 +0200

    - Added method for OrderManager which extracts the raw data from a result set, creates orders + orderItems and places them into an array list

--- src/Domain/Order/Order.java
+++ src/Domain/Order/Order.java
@@ -1,19 +1,20 @@
 package Domain.Order;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Order {
 
     private int ID;
-    private LocalDate startDate;
-    private LocalDate endDate;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
     private String status;
     private int deliveryPointID;
     private int customerID;
     private ArrayList<OrderItem> items;
 
-    public Order(int ID, LocalDate startDate, LocalDate endDate, String status, int deliveryPointID, int customerID){
+    public Order(int ID, LocalDateTime startDate, LocalDateTime endDate, String status, int deliveryPointID, int customerID){
         this.ID =  ID;
         this.startDate = startDate;
         this.endDate = endDate;
@@ -30,5 +31,15 @@ public class Order {
 
     public ArrayList<OrderItem> getOrderItems(){
         return this.items;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderID: " + this.ID + " Start date: " + this.startDate + " End date " + this.endDate +
+                " Status: " + this.status + " DeliveryPointID " + this.deliveryPointID + " CustomerID " + this.customerID;
     }
 }
