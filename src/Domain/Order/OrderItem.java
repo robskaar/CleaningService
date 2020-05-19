0c7746bce7604d62b2465db694df4ae8785495c1
JacobBonefeld <jacobbonefeld@live.dk>
2020-05-20 01:04:57 +0200

    - Added method for OrderManager which extracts the raw data from a result set, creates orders + orderItems and places them into an array list

--- src/Domain/Order/OrderItem.java
+++ src/Domain/Order/OrderItem.java
@@ -1,19 +1,33 @@
 package Domain.Order;
 
+import java.time.LocalDateTime;
+
 public class OrderItem {
 
     private boolean isWashed;
     private int ID;
-    private String type;
+    private int orderID;
+    private int laundryItemID;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
 
-    public OrderItem(boolean isWashed, int ID, String type){
+    public OrderItem(int ID, int laundryItemID, int orderID, boolean isWashed, LocalDateTime startDateTime, LocalDateTime endDateTime){
         this.isWashed = isWashed;
         this.ID =  ID;
-        this.type = type;
+        this.laundryItemID = laundryItemID;
+        this.orderID = orderID;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
     }
 
     public void updateStatus(boolean status){
 
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + ID + " OrderID: " + orderID + " laundryItemID " + laundryItemID + " isWashed " + isWashed +
+                " start date " + startDateTime + " end date " + endDateTime;
     }
 
     public boolean isWashed() {
@@ -24,7 +38,5 @@ public class OrderItem {
         return ID;
     }
 
-    public String getType() {
-        return type;
-    }
+
 }
