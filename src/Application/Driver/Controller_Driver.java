73f924dedd433d2f8f2df28cebc603b49c959443
JacobBonefeld <jacobbonefeld@live.dk>
2020-05-19 11:44:06 +0200

    - Added initial design for Driver GUI - Added OrderManager, Order and OrderItem skeleton

--- src/Application/Driver/Controller_Driver.java
+++ src/Application/Driver/Controller_Driver.java
@@ -1,22 +1,6 @@
 package Application.Driver;
 
 import Application.general.Controller_Application;
-import Domain.Managers.OrderManager;
-import Domain.Order.Order;
-import Domain.Order.OrderItem;
-
-import java.util.ArrayList;
 
 public class Controller_Driver extends Controller_Application {
-
-    public void execFunction(){
-        ArrayList<Order> orders = OrderManager.getRouteOrders(1);
-
-        System.out.println(orders.get(1).toString());
-
-        for(OrderItem oi : orders.get(1).getOrderItems()){
-            System.out.println(oi.toString());
-        }
-
-    }
 }
