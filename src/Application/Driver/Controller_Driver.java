f9bb0797e2beb14808a5b9856d5911b64b85322c
JacobBonefeld <jacobbonefeld@live.dk>
2020-05-20 08:53:55 +0200

    - Formatting

--- src/Application/Driver/Controller_Driver.java
+++ src/Application/Driver/Controller_Driver.java
@@ -10,13 +10,6 @@ import java.util.ArrayList;
 public class Controller_Driver extends Controller_Application {
 
     public void execFunction(){
-        ArrayList<Order> orders = OrderManager.getRouteOrders(1);
-
-        System.out.println(orders.get(1).toString());
-
-        for(OrderItem oi : orders.get(1).getOrderItems()){
-            System.out.println(oi.toString());
-        }
-
+        ArrayList<Order> orders = OrderManager.getRouteOrders(3);
     }
 }
