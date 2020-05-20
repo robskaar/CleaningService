87969ae7d9d3e0f69ae6d62db54adbd926f93a51
JacobBonefeld <55693790+JacobBonefeld@users.noreply.github.com>
2020-05-20 08:55:25 +0200

    Feature driver (#2)
    * - Added method for OrderManager which extracts the raw data from a result set, creates orders + orderItems and places them into an array list
    
    * - Formatting

--- src/Application/Driver/Controller_Driver.java
+++ src/Application/Driver/Controller_Driver.java
@@ -1,6 +1,15 @@
 package Application.Driver;
 
 import Application.general.Controller_Application;
+import Domain.Managers.OrderManager;
+import Domain.Order.Order;
+import Domain.Order.OrderItem;
+
+import java.util.ArrayList;
 
 public class Controller_Driver extends Controller_Application {
+
+    public void execFunction(){
+        ArrayList<Order> orders = OrderManager.getRouteOrders(3);
+    }
 }
--- src/Domain/Managers/OrderManager.java
+++ src/Domain/Managers/OrderManager.java
@@ -1,38 +1,126 @@
 package Domain.Managers;
 
 import Domain.Order.Order;
+import Domain.Order.OrderItem;
+import Foundation.Database.DB;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class OrderManager {
 
-    public static ArrayList<Order> getAllOrders(){
+    // Used to format SQL DateTime to Java LocalDateTime
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
+
+    public static ArrayList<Order> getAllOrders() {
 
         return new ArrayList<>();
     }
 
-    public static ArrayList<Order> getCustomerOrders(int customerID){
+    public static ArrayList<Order> getCustomerOrders(int customerID) {
 
         return new ArrayList<>();
     }
 
-    public static ArrayList<Order> getRouteOrders(int routeID){
+    /**
+     * @param routeID Id of the route where driver needs to pick-up/deliver orders
+     * @return Returns array list with orders from the route
+     */
+    public static ArrayList<Order> getRouteOrders(int routeID) {
+
+        DB.selectSQL("SELECT * FROM getRouteOrder(" + routeID + ")");
+
+        return convertResultSetToArrayList();
+    }
+
+    public static ArrayList<Order> getCentralOrders() {
 
         return new ArrayList<>();
     }
 
-    public static ArrayList<Order> getCentralOrders(){
-
-        return new ArrayList<>();
-    }
-
-    public static void updateOrderDB(Order order){
+    public static void updateOrderDB(Order order) {
 
 
     }
 
+    /**
+     * Only use after making a SQL select query that contains orders
+     * <p>
+     * This method takes the result set from the DB Class and converts the raw data into
+     * an array list which contains instances of orders
+     *
+     * @return Returns an array list with orders
+     */
+    private static ArrayList<Order> convertResultSetToArrayList() {
 
+        // Stores all orders from result set
+        ArrayList<Order> orders = new ArrayList<>();
+        // Temporary value used to check for null before parsing
+        String temp;
+        // Data uses to assert that there is more data
+        String data;
 
+        do {
+            data = DB.getData();
+            if (data.equals("|ND|")) {
+                break;
+            } else {
 
+                int orderID = Integer.parseInt(data);
+                int customerID = Integer.parseInt(DB.getData());
+                LocalDateTime startDate = LocalDateTime.parse(DB.getData(), formatter);
+                LocalDateTime endDate = null;
 
+                // Asserts that end date != null
+                if (!(temp = DB.getData()).equals("null")) {
+                    endDate = LocalDateTime.parse(temp, formatter);
+                }
+
+                String status = DB.getData();
+                int deliveryPointID = Integer.parseInt(DB.getData());
+
+                // Adds the order to the array list
+                orders.add(new Order(orderID, startDate, endDate, status, deliveryPointID, customerID));
+            }
+
+        } while (true);
+
+        addOrderItems(orders);
+
+        return orders;
+    }
+
+    /**
+     * This method adds all the order items from the database into the array list of orders
+     *
+     * @param orders Array list of the orders you want to add order items to
+     */
+    private static void addOrderItems(ArrayList<Order> orders) {
+
+        String temp;
+        boolean isWashed;
+        int orderItemID;
+        int orderID;
+        int laundryItemID;
+        LocalDateTime startDateTime;
+        LocalDateTime endDateTime = null;
+
+        for (Order order : orders) {
+            DB.selectSQL("SELECT * FROM getOrderItem(" + order.getID() + ")");
+
+            if (!(temp = DB.getData()).equals("|ND|")) {
+                orderItemID = Integer.parseInt(temp);
+                laundryItemID = Integer.parseInt(DB.getData());
+                orderID = Integer.parseInt(DB.getData());
+                isWashed = Boolean.parseBoolean(DB.getData());
+                startDateTime = LocalDateTime.parse(DB.getData(), formatter);
+
+                if (!(temp = DB.getData()).equals("null")) {
+                    endDateTime = LocalDateTime.parse(temp, formatter);
+                }
+                order.getOrderItems().add(new OrderItem(orderItemID, laundryItemID, orderID, isWashed, startDateTime, endDateTime));
+            }
+        }
+    }
 }
