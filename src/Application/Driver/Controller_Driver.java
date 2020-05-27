a57c36959220d8b6a29bb0af0ee33d3a5e512574
JacobBonefeld <jacobbonefeld@live.dk>
2020-05-27 10:25:31 +0200

    - Java Doc and comments

--- src/Application/Driver/Controller_Driver.java
+++ src/Application/Driver/Controller_Driver.java
@@ -36,10 +36,6 @@ public class Controller_Driver extends Controller_Application implements Initial
 
     @FXML
     private BorderPane root;
-    @FXML
-    private Button routeConfirm;
-    @FXML
-    private Button centralConfirm;
     /*
     FXML components from Central Orders pane
      */
@@ -57,6 +53,8 @@ public class Controller_Driver extends Controller_Application implements Initial
     private TableColumn<Integer, OrderItem> columnItemID;
     @FXML
     private TableColumn<Boolean, OrderItem> columnConfirm;
+    @FXML
+    private Button centralConfirm;
 
     /*
     FXML components from route orders pane
@@ -83,11 +81,17 @@ public class Controller_Driver extends Controller_Application implements Initial
     private TableColumn<Boolean, OrderItem> columnConfirmItem;
     @FXML
     private Label routeLabel;
+    @FXML
+    private Button routeConfirm;
 
     private static TableView<Order> currentOrderTable = null;
     private static TableView<OrderItem> currentItemsTable = null;
     private static Order selectedOrder = null;
     private static int currentRoute;
+    // Order statuses
+    private static final int AT_DELIVERY_POINT_READY_FOR_TRANSIT = 1;
+    private static final int AT_CLEANING_CENTRAL_READY_FOR_TRANSIT = 4;
+    private static final int IN_TRANSIT_TO_DELIVERY_POINT = 5;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -97,20 +101,34 @@ public class Controller_Driver extends Controller_Application implements Initial
         initCentralItemTable();
     }
 
+    /**
+     * Sets up order table for the laundry central
+     */
     private void initCentralOrderTable() {
-        centralOrderTable.setPlaceholder(getOnEmptyLabel("No more orders"));
-
+        // Set column width to fill entire table and set property for observable object
         columnOrderID.prefWidthProperty().bind(centralOrderTable.widthProperty());
         columnOrderID.setCellValueFactory(new PropertyValueFactory<>("ID"));
 
-        centralOrderTable.setItems(OrderManager.getRouteOrders(currentRoute, 4));
+        // Fetch orders from database and display
+        centralOrderTable.setItems(OrderManager.getRouteOrders(currentRoute, AT_CLEANING_CENTRAL_READY_FOR_TRANSIT));
+
+        // Set text to show if table is empty
+        setOnEmptyLabel("No more orders", centralOrderTable);
+
+        // Setup listener for when a row is selected
         setSelectListener(centralOrderTable, centralItemTable);
     }
 
+    /**
+     * Sets up item table for the laundry central
+     */
     private void initCentralItemTable() {
-        centralItemTable.setPlaceholder(getOnEmptyLabel("Select Order"));
+        // Sets text to be displayed when the table is empty
+        setOnEmptyLabel("Select Order", centralItemTable);
+        // Make table rows unselectable
         centralItemTable.setSelectionModel(null);
 
+        // Set object property and column width
         columnItemID.prefWidthProperty().bind(centralOrderTable.widthProperty().divide(4).multiply(3));
         columnItemID.setCellValueFactory(new PropertyValueFactory<>("ID"));
 
@@ -118,62 +136,97 @@ public class Controller_Driver extends Controller_Application implements Initial
         columnConfirm.setCellValueFactory(new PropertyValueFactory<>("checkBox"));
     }
 
+    /**
+     * Sets up table that shows delivery points
+     */
     private void initDeliveryPointTable() {
+
+        // Sets up columns property for observable object and column width
         columnDeliveryPoint.setCellValueFactory(new PropertyValueFactory<>("name"));
         columnDeliveryPoint.prefWidthProperty().bind(deliveryPointTable.widthProperty().divide(10).multiply(9));
 
         columnDeliveryConfirm.setCellValueFactory(new PropertyValueFactory<>("checkBox"));
         columnDeliveryConfirm.prefWidthProperty().bind(deliveryPointTable.widthProperty().divide(10));
 
-        deliveryPointTable.setItems(DeliveryPointManager.getRouteDeliveryPoints(1));
+        // Fetch delivery points from current route from database
+        deliveryPointTable.setItems(DeliveryPointManager.getRouteDeliveryPoints(currentRoute));
+
+        // Sets up listener for when a delivery point is selected
         deliveryPointTable.getSelectionModel().selectedItemProperty().addListener((observableValue, oldDeliveryPoint, newDeliveryPoint) -> {
 
+            // Show corresponding orders
             showDeliveryPointOrders();
 
+            // If there is no orders, then auto confirm delivery point
             if (pickUpTable.getItems().isEmpty() && deliverTable.getItems().isEmpty()) {
                 newDeliveryPoint.confirmAllOrdersDone();
             }
         });
-
     }
 
+    /**
+     * Sets up route deliver- and pick up table
+     */
     private void initRouteOrderTables() {
-        columnDeliverOrders.setCellValueFactory(new PropertyValueFactory<>("ID"));
-        columnDeliverOrders.prefWidthProperty().bind(deliverTable.widthProperty());
+        // Add listener for selection of row
         setSelectListener(deliverTable, routeOrderItemsTable);
-        deliverTable.setPlaceholder(getOnEmptyLabel("Choose Delivery Point"));
-
-        columnPickUp.setCellValueFactory(new PropertyValueFactory<>("ID"));
-        columnPickUp.prefWidthProperty().bind(pickUpTable.widthProperty());
         setSelectListener(pickUpTable, routeOrderItemsTable);
-        pickUpTable.setPlaceholder(getOnEmptyLabel("Choose Delivery Point"));
+
+        // Sets up property to get from observable object
+        columnDeliverOrders.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        columnPickUp.setCellValueFactory(new PropertyValueFactory<>("ID"));
+
+        // Sets column width to fill entire table
+        columnDeliverOrders.prefWidthProperty().bind(deliverTable.widthProperty());
+        columnPickUp.prefWidthProperty().bind(pickUpTable.widthProperty());
+
+        // Sets text to show when table is empty
+        setOnEmptyLabel("Choose Delivery Point", deliverTable);
+        setOnEmptyLabel("Choose Delivery Point", pickUpTable);
     }
 
+    /**
+     * Sets of table that view route order items
+     */
     private void initRouteItemsTable() {
+        // Items can not be selected (Only use checkbox)
         routeOrderItemsTable.setSelectionModel(null);
+
+        // Sets which properties to get from the observable objects
         columnOrderItem.setCellValueFactory(new PropertyValueFactory<>("ID"));
         columnConfirmItem.setCellValueFactory(new PropertyValueFactory<>("checkBox"));
-        routeOrderItemsTable.setPlaceholder(getOnEmptyLabel("Choose order"));
+
+        // Sets text to show if table is empty
+        setOnEmptyLabel("Choose Order", routeOrderItemsTable);
     }
 
+    /**
+     * Method called when a delivery point is selected
+     * Shows all orders from the delivery point
+     */
     public void showDeliveryPointOrders() {
         routeOrderItemsTable.getItems().clear();
+
+        // Gets ID from selected delivery point
         int ID = Integer.parseInt(deliveryPointTable.getSelectionModel().getSelectedItem().getID());
 
-        // Insert data to table views
-        deliverTable.setItems(OrderManager.getDeliveryPointOrders(ID, 5));
-        pickUpTable.setItems(OrderManager.getDeliveryPointOrders(ID, 1));
+        // Fetch data from DB and insert in table views
+        deliverTable.setItems(OrderManager.getDeliveryPointOrders(ID, IN_TRANSIT_TO_DELIVERY_POINT));
+        pickUpTable.setItems(OrderManager.getDeliveryPointOrders(ID, AT_DELIVERY_POINT_READY_FOR_TRANSIT));
 
         // Set table view text if empty
         if (deliverTable.getItems().isEmpty()) {
-            deliverTable.setPlaceholder(getOnEmptyLabel("No Orders"));
+            setOnEmptyLabel("No Orders", deliverTable);
         }
         if (pickUpTable.getItems().isEmpty()) {
-            pickUpTable.setPlaceholder(getOnEmptyLabel("No Orders"));
+            setOnEmptyLabel("No Orders", pickUpTable);
         }
-
     }
 
+    /**
+     * Method called when user clicks confirm order
+     * Checks if all items from the order has been confirmed
+     */
     public void confirmOrder() {
 
         boolean isAllItemsConfirmed = true;
@@ -193,10 +246,12 @@ public class Controller_Driver extends Controller_Application implements Initial
                 selectedOrder.updateStatus(newStatus);
                 OrderManager.updateOrderDB(selectedOrder);
 
+                // Show message that order has been confirmed
                 showMessage("Confirmed", Color.web("#2ECC71"));
+
                 // Remove confirmed order from the list
                 if (currentOrderTable.equals(centralOrderTable)) {
-                    centralOrderTable.setItems(OrderManager.getRouteOrders(currentRoute, 4));
+                    centralOrderTable.setItems(OrderManager.getRouteOrders(currentRoute, AT_CLEANING_CENTRAL_READY_FOR_TRANSIT));
                     // Disable and change text on confirm button
                     centralConfirm.setDisable(true);
                     centralConfirm.setText("No orders selected");
@@ -209,7 +264,7 @@ public class Controller_Driver extends Controller_Application implements Initial
                     isDeliveryPointDone();
                 }
 
-                // Remove order items from list
+                // Remove order items from table
                 currentItemsTable.getItems().clear();
 
                 // Select next order on list
@@ -226,56 +281,61 @@ public class Controller_Driver extends Controller_Application implements Initial
 
     }
 
+    /**
+     * Method called when user clicks start route
+     * Checks if all orders has been picked up from the laundry central
+     */
     public void startRoute() {
 
         if (centralOrderTable.getItems().isEmpty()) {
+            // Change view to route scene if all orders are confirmed
             changeView();
         } else {
+            // Show warning message if some orders has not been confirmed
             showMessage("Please confirm all orders", Color.web("#E74C3C"));
         }
-        /*
-        boolean isAllOrdersConfirmed = true;
-
-        if (!centralOrderTable.getItems().isEmpty()) {
-            for (Order order : centralOrderTable.getItems()) {
-                if (!(order.getStatus().equals("5"))) {
-                    isAllOrdersConfirmed = false;
-                    showMessage("Please confirm all orders",Color.web("#E74C3C"));
-                    break;
-                }
-            }
-        }
-
-        if (isAllOrdersConfirmed) {
-            changeView();
-        }
-
-         */
 
     }
 
+    /**
+     * Method called when user presses end route
+     * Checks if all delivery points has all orders confirmed
+     * Logs out if true
+     */
     public void endRoute() {
+
         boolean isAllDeliveryPointsConfirmed = true;
 
-        for(DeliveryPoint dp : deliveryPointTable.getItems()){
-            if(!dp.getCheckBox().isSelected()){
+        // Checks if all delivery points is confirmed
+        for (DeliveryPoint dp : deliveryPointTable.getItems()) {
+            if (!dp.getCheckBox().isSelected()) {
                 isAllDeliveryPointsConfirmed = false;
+
+                // Show warning message if user failed to confirm all delivery points
                 showMessage("Please confirm all orders", Color.web("#E74C3C"));
                 break;
             }
         }
 
-        if(isAllDeliveryPointsConfirmed){
+        // Log off if all delivery points is confirmed
+        if (isAllDeliveryPointsConfirmed) {
             AccountManager.logOff();
         }
     }
 
+    /**
+     * Checks if all orders from a delivery point has been confirmed
+     * Selects the checkbox on delivery point table if true
+     */
     private void isDeliveryPointDone() {
         if (pickUpTable.getItems().isEmpty() && deliverTable.getItems().isEmpty()) {
             deliveryPointTable.getSelectionModel().getSelectedItem().confirmAllOrdersDone();
         }
     }
 
+    /**
+     * Changes the scene from picking up orders from central, to the route scene
+     */
     private void changeView() {
         centralOrders.setVisible(false);
         routeOrders.setVisible(true);
@@ -286,12 +346,24 @@ public class Controller_Driver extends Controller_Application implements Initial
         initRouteItemsTable();
     }
 
-    private Label getOnEmptyLabel(String text) {
+    /**
+     * Sets the text to be displayed, when a table is empty
+     *
+     * @param text  The text to be displayed
+     * @param table The table
+     */
+    private void setOnEmptyLabel(String text, TableView table) {
         Label onEmptyLabel = new Label(text);
         onEmptyLabel.getStyleClass().add("onEmptyLabel");
-        return onEmptyLabel;
+        table.setPlaceholder(onEmptyLabel);
     }
 
+    /**
+     * Creates a listener for order tables. It listens for a selection of a row.
+     *
+     * @param tableOrder The order table to listen on
+     * @param tableItem  The corresponding table, where order table will show its items
+     */
     private void setSelectListener(TableView<Order> tableOrder, TableView<OrderItem> tableItem) {
 
         tableOrder.getSelectionModel().selectedItemProperty().addListener((observableValue, oldOrder, newOrder) -> {
@@ -300,19 +372,20 @@ public class Controller_Driver extends Controller_Application implements Initial
                 selectedOrder = newOrder;
                 currentOrderTable = tableOrder;
                 currentItemsTable = tableItem;
+
+                // Shows the order items
                 tableItem.setItems(FXCollections.observableArrayList(selectedOrder.getOrderItems()));
 
                 // Setting text on confirm button
-
                 if (tableOrder.equals(centralOrderTable)) {
                     centralConfirm.setDisable(false);
                     centralConfirm.setText("Confirm pick up of order #" + selectedOrder.getID());
                 } else if (tableOrder.equals(pickUpTable)) {
-                    deliverTable.getSelectionModel().clearSelection();
+                    deliverTable.getSelectionModel().clearSelection(); // Assert that only deliver table is selected
                     routeConfirm.setDisable(false);
                     routeConfirm.setText("Confirm pick up of order #" + selectedOrder.getID());
                 } else if (tableOrder.equals(deliverTable)) {
-                    pickUpTable.getSelectionModel().clearSelection();
+                    pickUpTable.getSelectionModel().clearSelection(); // Assert that only pick up table is selected
                     routeConfirm.setDisable(false);
                     routeConfirm.setText("Confirm delivery of order #" + selectedOrder.getID());
                 }
@@ -321,32 +394,46 @@ public class Controller_Driver extends Controller_Application implements Initial
 
     }
 
+    /**
+     * This method creates a pop up box, which informs user of a message
+     *
+     * @param message Text to be displayed on screen
+     * @param color   Color of the pop up box
+     */
     private void showMessage(String message, Color color) {
 
+        // The stack makes it possible to add text on top of a rectangle
         StackPane stack = new StackPane();
         stack.setLayoutX(510);
         stack.setLayoutY(678);
         stack.setOpacity(0);
 
+        // The text to be displayed
         Text text = new Text(message);
         text.setFont(Font.font(20));
 
+        // The pop up box
         Rectangle rectangle = new Rectangle(832, 40, color);
 
         stack.getChildren().addAll(rectangle, text);
         root.getChildren().add(stack);
 
+        // Transition to fade in the pop up
         FadeTransition fadeIn = new FadeTransition(Duration.millis(500), stack);
         fadeIn.setFromValue(0);
         fadeIn.setToValue(1);
         fadeIn.play();
 
+        // When faded in, create a fade out transition
         fadeIn.setOnFinished(event -> {
+
             FadeTransition fadeOut = new FadeTransition(Duration.millis(1500), stack);
             fadeOut.setFromValue(1);
             fadeOut.setToValue(0);
             fadeOut.setDelay(Duration.millis(300));
             fadeOut.play();
+
+            // When fade out is done, remove the stack to get garbage collected
             fadeOut.setOnFinished(event1 -> {
                 root.getChildren().remove(stack);
             });
